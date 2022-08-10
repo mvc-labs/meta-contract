a9e3be013351f8b8aca4f9709ad7be71b9611f1a
Vincent Tse <river_run@126.com>
2022-08-10 11:21:47 +0800

    fix style

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,46 @@
-const ts = require("gulp-typescript");
-const tsProject = ts.createProject("tsconfig.json");
-const gulp = require("gulp");
-const browserify = require("browserify");
-const source = require("vinyl-source-stream");
-const tsify = require("tsify");
-const watchify = require("watchify");
-const gutil = require("gulp-util");
-const uglify = require("gulp-uglify");
-const sourcemaps = require("gulp-sourcemaps");
-const buffer = require("vinyl-buffer");
-const clean = require("gulp-clean"); //清理文件或文件夹
-const standalonify = require("standalonify");
-var merge = require("merge2");
-gulp.task("clean", function () {
-  return gulp.src("dist/*", { read: false }).pipe(clean());
-});
+const ts = require('gulp-typescript')
+const tsProject = ts.createProject('tsconfig.json')
+const gulp = require('gulp')
+const browserify = require('browserify')
+const source = require('vinyl-source-stream')
+const tsify = require('tsify')
+const watchify = require('watchify')
+const gutil = require('gulp-util')
+const uglify = require('gulp-uglify')
+const sourcemaps = require('gulp-sourcemaps')
+const buffer = require('vinyl-buffer')
+const clean = require('gulp-clean') //清理文件或文件夹
+const standalonify = require('standalonify')
+var merge = require('merge2')
+gulp.task('clean', function () {
+  return gulp.src('dist/*', { read: false }).pipe(clean())
+})
 
-gulp.task("tsc", function () {
-  var tsResult = tsProject.src().pipe(tsProject());
-  return merge([
-    tsResult.dts.pipe(gulp.dest("dist")),
-    tsResult.js.pipe(gulp.dest("dist")),
-  ]);
-});
-gulp.task("copy_file", function () {
+gulp.task('tsc', function () {
+  var tsResult = tsProject.src().pipe(tsProject())
+  return merge([tsResult.dts.pipe(gulp.dest('dist')), tsResult.js.pipe(gulp.dest('dist'))])
+})
+gulp.task('copy_file', function () {
   return gulp
-    .src(["./src/**/*.js", "./src/**/*.json", "./src/**/*.d.ts"], {
-      base: "./src",
+    .src(['./src/**/*.js', './src/**/*.json', './src/**/*.d.ts'], {
+      base: './src',
     })
-    .pipe(gulp.dest("dist"));
-});
-gulp.task("browserify", function () {
-  tsProject.config.exclude = "";
+    .pipe(gulp.dest('dist'))
+})
+gulp.task('browserify', function () {
+  tsProject.config.exclude = ''
   return browserify({
-    basedir: ".",
+    basedir: '.',
     debug: true,
-    entries: ["src/index.browser.ts"],
+    entries: ['src/index.browser.ts'],
     cache: {},
     packageCache: {},
     // fullPaths: true,
   })
     .plugin(tsify, tsProject.config)
-    .plugin(standalonify, { name: "sensible" })
+    .plugin(standalonify, { name: 'sensible' })
     .bundle()
-    .pipe(source("metaContract.browser.min.js"))
+    .pipe(source('metaContract.browser.min.js'))
     .pipe(buffer())
     .pipe(
       sourcemaps.init({
@@ -51,16 +48,16 @@ gulp.task("browserify", function () {
       })
     )
     .pipe(uglify())
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest("dist"));
-});
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'))
+})
 
 function dev() {
-  gulp.watch(["src/**/*.ts", "src/**/*.d.ts"], gulp.series("tsc"));
-  gulp.watch(["src/**/*.js"], gulp.series("tsc"));
+  gulp.watch(['src/**/*.ts', 'src/**/*.d.ts'], gulp.series('tsc'))
+  gulp.watch(['src/**/*.js'], gulp.series('tsc'))
 }
 
 // gulp.task("default", gulp.series("clean", "tsc", "copy_file", "browserify"));
-gulp.task("default", gulp.series("clean", "tsc", "copy_file"));
+gulp.task('default', gulp.series('clean', 'tsc', 'copy_file'))
 // gulp.task("dev", gulp.series("clean", "tsc", "copy_file", "browserify", dev));
-gulp.task("dev", gulp.series("clean", "tsc", "copy_file", dev));
+gulp.task('dev', gulp.series('clean', 'tsc', 'copy_file', dev))
